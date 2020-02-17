f757688b9300311d3aa54e08ae04e582886e41de
Jan Pfeiffer <40457584@live.napier.ac.uk>
2020-02-17 11:56:58 +0000

    Debug Output fixed.

--- src/main/java/com/napier/semgroup/app/PopulationStatsApplication.java
+++ src/main/java/com/napier/semgroup/app/PopulationStatsApplication.java
@@ -14,12 +14,7 @@ public class PopulationStatsApplication {
      * @param args The Arguments
      */
     public static void main (String[] args){
-        /*
-         * Start the Application via the docker compose file. The DB had to be started once.
-         * When you change something in the code and want to test it, make sure you have packed the jar over marven
-         * and deleted the old image in docker, otherwise the new code will not be executed.
-         * Do this steps all the time you want to try new code snippets.
-         */
+
         DatabaseConnection dbcon = new MySQLConnectionHandler(); //new object
         dbcon.connect(); //connection to database
         BusinessLogic businessLogic = new BusinessLogic(dbcon); //new object
