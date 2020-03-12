65b87b23494f294b4558a3737ae4f50c27e76823
Jan Pfeiffer <40457584@live.napier.ac.uk>
2020-03-12 13:31:41 +0000

    Test example added.

--- src/test/java/com/napier/semgroup/PopStatsAppIntegrationTests.java
+++ src/test/java/com/napier/semgroup/PopStatsAppIntegrationTests.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PopStatsAppIntegrationTests {
+
     static PopulationStatsApplication a;
     static DatabaseConnection databaseConnection;
     static BusinessLogic businessLogic;
