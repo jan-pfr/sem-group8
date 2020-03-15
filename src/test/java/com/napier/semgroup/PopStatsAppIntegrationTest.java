f0d3f827e480f22f25867393b93c6a35a5647065
redspah <redspah@gmail.com>
2020-03-15 12:11:02 +0000

    Adding tests to cover all the functions so far

--- src/test/java/com/napier/semgroup/PopStatsAppIntegrationTest.java
+++ src/test/java/com/napier/semgroup/PopStatsAppIntegrationTest.java
@@ -82,6 +82,119 @@ public class PopStatsAppIntegrationTest {
     }
 
     @Test
+    void getAllCitiesOrganizedByPopulation() {
+        for (City c : businessLogic.getAllCitiesOrganizedByPopulation()) {
+            System.out.println(c);
+        }
+    }
+
+    @Test
+    void getAllCitiesInContinentOrganizedByPopulation() {
+        for (City c : businessLogic.getAllCitiesInContinentOrganizedByPopulation("South America")) {
+            System.out.println(c);
+        }
+    }
+
+    @Test
+    void getAllCitiesInRegionOrganizedByPopulation() {
+        for (City c : businessLogic.getAllCitiesInRegionOrganizedByPopulation("Middle East")) {
+            System.out.println(c);
+        }
+    }
+
+    @Test
+    void getAllCitiesInCountryOrganizedByPopulation() {
+        for (City c : businessLogic.getAllCitiesInCountryOrganizedByPopulation("Poland")) {
+            System.out.println(c);
+        }
+    }
+
+    @Test
+    void getAllCitiesInDistrictOrganizedByPopulation() {
+        for (City c : businessLogic.getAllCitiesInDistrictOrganizedByPopulation("Hokkaido")) {
+            System.out.println(c);
+        }
+    }
+
+    @Test
+    void getNCitiesOrganizedByPopulation() {
+        for (City c : businessLogic.getNCitiesOrganizedByPopulation(12)) {
+            System.out.println(c);
+        }
+    }
+
+    @Test
+    void getNCitiesInContinentOrganizedByPopulation() {
+        for (City c : businessLogic.getNCitiesInContinentOrganizedByPopulation("North America", 17)) {
+            System.out.println(c);
+        }
+    }
+
+    @Test
+    void getNCitiesInRegionOrganizedByPopulation() {
+        for (City c : businessLogic.getNCitiesInRegionOrganizedByPopulation("Baltic Countries", 5)) {
+            System.out.println(c);
+        }
+    }
+
+    @Test
+    void getNCitiesInCountryOrganizedByPopulation() {
+        for (City c : businessLogic.getNCitiesInCountryOrganizedByPopulation("France", 8)) {
+            System.out.println(c);
+        }
+    }
+
+    @Test
+    void getNCitiesInDistrictOrganizedByPopulation() {
+        for (City c : businessLogic.getNCitiesInDistrictOrganizedByPopulation("England", 11)) {
+            System.out.println(c);
+        }
+    }
+
+    @Test
+    void getAllCapitalCitiesOrganizedByPopulation() {
+        for (City c : businessLogic.getAllCapitalCitiesOrganizedByPopulation()) {
+            System.out.println(c);
+        }
+    }
+
+    @Test
+    void getAllCapitalCitiesInContinentOrganizedByPopulation() {
+        for (City c : businessLogic.getAllCapitalCitiesInContinentOrganizedByPopulation("Oceania")) {
+            System.out.println(c);
+        }
+    }
+
+    @Test
+    void getAllCapitalCitiesInRegionOrganizedByPopulation() {
+        for (City c : businessLogic.getAllCapitalCitiesInRegionOrganizedByPopulation("Central Africa")) {
+            System.out.println(c);
+        }
+    }
+
+    @Test
+    void getNCapitalCitiesOrganizedByPopulation() {
+        for (City c : businessLogic.getNCapitalCitiesOrganizedByPopulation(7)) {
+            System.out.println(c);
+        }
+    }
+
+    @Test
+    void getNCapitalCitiesInContinentOrganizedByPopulation() {
+        for (City c : businessLogic.getNCapitalCitiesInContinentOrganizedByPopulation("Europe", 9)) {
+            System.out.println(c);
+        }
+    }
+
+    @Test
+    void getNCapitalCitiesInRegionOrganizedByPopulation() {
+        for (City c : businessLogic.getNCapitalCitiesInRegionOrganizedByPopulation("Polynesia", 6)) {
+            System.out.println(c);
+        }
+    }
+    
+
+    @Test
     void disconnect(){
         mySQLConnectionHandler.disconnect();
        mySQLConnectionHandler.connect("localhost:33080");
