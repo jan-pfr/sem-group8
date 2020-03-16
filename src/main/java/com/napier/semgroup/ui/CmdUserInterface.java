2753dd42afb4c5cc61b27b97f559630d613cd193
caelumn <40457307@live.napier.ac.uk>
2020-03-16 13:09:19 +0000

    Menu-text added.

--- src/main/java/com/napier/semgroup/ui/CmdUserInterface.java
+++ src/main/java/com/napier/semgroup/ui/CmdUserInterface.java
@@ -2,18 +2,25 @@ package com.napier.semgroup.ui;
 
 import com.napier.semgroup.logic.BusinessLogic;
 
+import java.io.*;
+import java.util.*;
+import java.util.Properties;
+
 public class CmdUserInterface {
     private BusinessLogic businessLogic;
 
-    public CmdUserInterface(BusinessLogic businessLogic){
-        this.businessLogic = businessLogic;
-    }
+    //public CmdUserInterface(BusinessLogic businessLogic){
+        //this.businessLogic = businessLogic;
+    //}
+
 
     public void showMenu(){
         System.out.println("Welcome to to the Population Information System.");
         System.out.println("Please select one option of the menu provided below by typing the number:");
         System.out.println("_________________________________________________________________________\n");
-        System.out.println("#01: Get all Countries organized By Population");
+        for (int i = 0; i < 23; i++){
+
+        }
 
     }
     public void clearScreen() {
